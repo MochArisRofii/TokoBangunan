d035636f6c352a7237d0454d4bdeb78b6b2ee211
MochArisRofii <mochrofii827@gmail.com>
2024-11-26 14:12:49 +0700

    Update

--- koneksi.php
+++ koneksi.php
@@ -1,14 +1,18 @@
 <?php
-    $servername = "localhost";  // Ganti dengan server Anda jika diperlukan
-    $username = "root";         // Ganti dengan username database Anda
-    $password = "";             // Ganti dengan password database Anda
-    $dbname = "toko_bangunan";  // Ganti dengan nama database Anda
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "toko_bangunan";
 
-    // Buat koneksi
-    $conn = new mysqli($servername, $username, $password, $dbname);
+// Buat koneksi
+$conn = new mysqli(
+    $servername,
+    $username,
+    $password,
+    $dbname
+);
 
-    // Cek koneksi
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-?>
+// Cek koneksi
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
